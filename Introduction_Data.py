f7ca6c21326db2c5391093fdb17135405ad1db35
ChrisSynergy <41619140+ChrisSynergy@users.noreply.github.com>
2018-11-02 11:46:17 +0200

    Published from datascience.ibm.com

---
+++ Introduction_Data.py
@@ -0,0 +1,44 @@
+{
+    "nbformat_minor": 1, 
+    "cells": [
+        {
+            "source": "\n        \n<h1>Introduction/Business Problem:</h1>\n        \nA client wants to oepn a coffee shop but they don't know where to locate the coffee shop.\nThe client is struggling to find the exact spot or location where to open the coffee shop.\nThe client is targeting middle class to high class customers in between New York and Torono.\n        The client wants their coffee shop to be the best but also have reasonable prices.\n                            \nThis project aims to make it easy for the client to find a perfect spot to open their coffee shop,\nby providing all nearby locations or spots around New York and Toronto.\n                            \nThis project will try to build and make recommendations which will be a guiding line to the client.\n      \n                            ", 
+            "cell_type": "markdown", 
+            "metadata": {
+                "collapsed": true
+            }
+        }, 
+        {
+            "source": "<h1>Data:</h1>\n    \nThe data will be collected from Foursquare. The data will be about the municipalities in New York and Toronto\nwhich includes all the locations within New York and Toronto.\n\nThe data will allow us to inspect and find a suitable location or spot for the client to open their coffee shop.", 
+            "cell_type": "markdown", 
+            "metadata": {}
+        }, 
+        {
+            "execution_count": null, 
+            "cell_type": "code", 
+            "metadata": {}, 
+            "outputs": [], 
+            "source": ""
+        }
+    ], 
+    "metadata": {
+        "kernelspec": {
+            "display_name": "Python 3.5", 
+            "name": "python3", 
+            "language": "python"
+        }, 
+        "language_info": {
+            "mimetype": "text/x-python", 
+            "nbconvert_exporter": "python", 
+            "version": "3.5.5", 
+            "name": "python", 
+            "file_extension": ".py", 
+            "pygments_lexer": "ipython3", 
+            "codemirror_mode": {
+                "version": 3, 
+                "name": "ipython"
+            }
+        }
+    }, 
+    "nbformat": 4
+}
